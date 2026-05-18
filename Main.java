b71f5bef9b9de958804f4692f882fd7fe72f38b1
andyy1615 <andy.donoso@udla.edu.ec>
2026-05-18 14:15:22 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import vista.VentanaPrincipal; 
+import javax.swing.*; 
+
+public class Main { 
+    public static void main(String[] args) { 
+        try { 
+            UIManager.setLookAndFeel( 
+                UIManager .getSystemLookAndFeelClassName()); 
+            } 
+        catch (Exception e) { 
+            e.printStackTrace(); 
+        } 
+        
+        SwingUtilities.invokeLater(() -> { 
+            VentanaPrincipal ventana = new VentanaPrincipal(); 
+            ventana.setVisible(true); 
+        }); 
+    } 
+}
